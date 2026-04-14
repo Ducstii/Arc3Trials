6f4ecb23775e92d88f7ca69f2462d42117f5816c
Ducstii <elliott.danger.clark@gmail.com>
2026-04-14 11:25:57 -0400

    jnk

---
+++ Arc3Trials/Adreniline/HarassExtensions.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using CustomPlayerEffects;
+using LabApi.Features.Wrappers;
+using MEC;
+
+namespace Arc3Trials.Adreniline
+{
+    public static class HarassExtensions
+    {
+        extension(Player player)
+        {
+            public void Harrass(int duration)
+            {
+                player.EnableEffect<Concussed>(255, 10, true);
+                player.EnableEffect<Slowness>(255, 10, true);
+                Timing.RunCoroutine(FlashbangCoroutine(player, duration));
+            }
+        }
+
+        private static IEnumerator<float> FlashbangCoroutine(Player player, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                player.EnableEffect<Flashed>(255, 0.1f);
+                yield return Timing.WaitForSeconds(0.1f);
+                player.DisableEffect<Flashed>();
+                yield return Timing.WaitForSeconds(0.1f);
+            }
+        }
+    }
+}
